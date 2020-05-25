344fecc9dd25c36a65259671eca455f9ea8b3cf0
Brendan Molloy <brendan@bbqsrc.net>
2020-05-25 16:53:40 +0200

    First attempt at install-deps and build

--- lang/build/index.js
+++ lang/build/index.js
@@ -33,26 +33,81 @@ class Autotools {
         await this.runMake();
     }
 }
+function deriveInputs(inputs) {
+    const o = {};
+    for (const input in inputs) {
+        o[input] = JSON.parse(core.getInput(input));
+    }
+    return o;
+}
 async function run() {
     const githubWorkspace = process.env.GITHUB_WORKSPACE;
     if (githubWorkspace == null) {
         core.setFailed("GITHUB_WORKSPACE not set, failing.");
         return;
     }
+    const config = deriveInputs([
+        "fst",
+        "spellers",
+        "hyphenators",
+        "analysers",
+        "grammar-checkers",
+        "hyperminimalisation",
+        "reversed-intersect",
+        "two-step-intersect",
+        "speller-optimisation",
+        "backend-format",
+        "force-all-tools"
+    ]);
+    const flags = [
+        "--without-forrest",
+        "--disable-silent-rules",
+        "--without-xfst"
+    ];
+    if (config.fst.contains("foma")) {
+        flags.push("--with-foma");
+    }
+    if (!config.fst.contains("hfst")) {
+        flags.push("--without-hfst");
+    }
+    if (!config.analysers) {
+        flags.push("--disable-analysers");
+        flags.push("--disable-generators");
+        flags.push("--disable-transcriptors");
+    }
+    if (config.hyphenators) {
+        flags.push("--enable-fst-hyphenator");
+    }
+    if (config.spellers || config["grammar-checkers"]) {
+        flags.push("--enable-spellers");
+        flags.push("--disable-hfst-desktop-spellers");
+        flags.push("--enable-hfst-mobile-speller");
+    }
+    if (config["grammar-checkers"]) {
+        flags.push("--enable-grammarchecker");
+    }
+    if (config.hyperminimalisation) {
+        flags.push("--enable-hyperminimalisation");
+    }
+    if (config["reversed-intersect"]) {
+        flags.push("--enable-reversed-intersect");
+    }
+    if (config["twostep-intersect"]) {
+        flags.push("--enable-twostep-intersect");
+    }
+    if (config["backend-format"]) {
+        flags.push(`--with-backend-format=${config["backend-format"]}`);
+    }
+    if (config["minimised-spellers"]) {
+        flags.push("--enable-minimised-spellers");
+    }
     core.startGroup("Build giella-core and giella-shared");
     await shared_1.Bash.runScript("./autogen.sh && ./configure && make install", path.join(githubWorkspace, "giella-core"));
     await shared_1.Bash.runScript("./autogen.sh && ./configure && make install", path.join(githubWorkspace, "giella-shared"));
     core.endGroup();
     const builder = new Autotools(path.join(githubWorkspace, "lang"));
-    await builder.build([
-        "--without-forrest",
-        "--with-hfst",
-        "--without-xfst",
-        "--enable-reversed-intersect",
-        "--disable-hfst-desktop-spellers",
-        "--enable-spellers",
-        "--enable-hfst-mobile-speller"
-    ]);
+    core.debug(`Flags: ${flags}`);
+    await builder.build(flags);
     await shared_1.Bash.runScript("ls -lah tools/spellcheckers/", path.join(githubWorkspace, "lang"));
 }
 run().catch(err => {
--- lang/build/index.ts
+++ lang/build/index.ts
@@ -31,7 +31,17 @@ class Autotools {
         await this.runConfigure(flags)
         await this.runMake()
     }
-} 
+}
+
+function deriveInputs(inputs: string[]): { [key: string]: any } {
+    const o: { [key: string]: any } = {}
+
+    for (const input in inputs) {
+        o[input] = JSON.parse(core.getInput(input))
+    }
+
+    return o
+}
 
 async function run() {
     const githubWorkspace = process.env.GITHUB_WORKSPACE
@@ -41,6 +51,80 @@ async function run() {
         return
     }
 
+    const config = deriveInputs([
+        "fst",
+        "spellers",
+        "hyphenators",
+        "analysers",
+        "grammar-checkers",
+        "hyperminimalisation",
+        "reversed-intersect",
+        "two-step-intersect",
+        "speller-optimisation",
+        "backend-format",
+        "force-all-tools"
+    ])
+
+    const flags = [
+        "--without-forrest",
+        "--disable-silent-rules",
+        "--without-xfst"
+    ]
+
+    // General configuration
+
+    if (config.fst.contains("foma")) {
+        flags.push("--with-foma")
+    }
+
+    if (!config.fst.contains("hfst")) {
+        flags.push("--without-hfst")
+    }
+
+    if (!config.analysers) {
+        flags.push("--disable-analysers")
+        flags.push("--disable-generators")
+        flags.push("--disable-transcriptors")
+    }
+
+    if (config.hyphenators) {
+        flags.push("--enable-fst-hyphenator")
+    }
+
+    if (config.spellers || config["grammar-checkers"]) {
+        flags.push("--enable-spellers")
+        flags.push("--disable-hfst-desktop-spellers")
+        flags.push("--enable-hfst-mobile-speller")
+    }
+
+    if (config["grammar-checkers"]) {
+        flags.push("--enable-grammarchecker")
+    }
+
+    // Language-specific optimisations
+
+    if (config.hyperminimalisation) {
+        flags.push("--enable-hyperminimalisation")
+    }
+
+    if (config["reversed-intersect"]) {
+        flags.push("--enable-reversed-intersect")
+    }
+
+    if (config["twostep-intersect"]) {
+        flags.push("--enable-twostep-intersect")
+    }
+
+    if (config["backend-format"]) {
+        flags.push(`--with-backend-format=${config["backend-format"]}`)
+    }
+
+    if (config["minimised-spellers"]) {
+        flags.push("--enable-minimised-spellers")
+    }
+
+    // Begin build
+
     core.startGroup("Build giella-core and giella-shared")
     await Bash.runScript("./autogen.sh && ./configure && make install", path.join(githubWorkspace, "giella-core"))
     await Bash.runScript("./autogen.sh && ./configure && make install", path.join(githubWorkspace, "giella-shared"))
@@ -48,15 +132,8 @@ async function run() {
 
     const builder = new Autotools(path.join(githubWorkspace, "lang"))
 
-    await builder.build([
-        "--without-forrest",
-        "--with-hfst",
-        "--without-xfst",
-        "--enable-reversed-intersect",
-        "--disable-hfst-desktop-spellers",
-        "--enable-spellers",
-        "--enable-hfst-mobile-speller"
-    ])
+    core.debug(`Flags: ${flags}`)
+    await builder.build(flags)
 
     await Bash.runScript("ls -lah tools/spellcheckers/", path.join(githubWorkspace, "lang"))
 }
