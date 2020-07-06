d7e67e23f09e8d4b060108714a8b547209cfe52f
Brendan Molloy <brendan@bbqsrc.net>
2020-07-06 14:32:19 +0200

    Add Inno Setup file builder

---
+++ inno.js
@@ -0,0 +1,278 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+function stringFromInnoFile(input) {
+    let out = `Source: "${input.Source}"; DestDir: "${input.DestDir}"`;
+    if (input.Flags) {
+        out += "; Flags: ";
+        out += input.Flags.join(" ");
+    }
+    return out;
+}
+class InnoSetupBuilder {
+    constructor() {
+        this.data = {};
+    }
+    name(input) {
+        this.data.name = input;
+        return this;
+    }
+    version(input) {
+        this.data.version = input;
+        return this;
+    }
+    publisher(input) {
+        this.data.publisher = input;
+        return this;
+    }
+    url(input) {
+        this.data.url = input;
+        return this;
+    }
+    productCode(input) {
+        this.data.productCode = input;
+        return this;
+    }
+    defaultDirName(input) {
+        this.data.defaultDirName = input;
+        return this;
+    }
+    files(callback) {
+        this.data.files = callback(new InnoSetupFilesBuilder());
+        return this;
+    }
+    code(callback) {
+        this.data.code = callback(new InnoSetupCodeBuilder());
+        return this;
+    }
+    build() {
+        for (const key of ["name", "version", "url", "productCode", "defaultDirName"]) {
+            if (this.data[key] == null) {
+                throw new Error(`Missing key "${key}" for Inno Setup builder`);
+            }
+        }
+        const { name, version, publisher, url, productCode, defaultDirName } = this.data;
+        const setup = Object.entries({
+            AppId: `{${productCode}`,
+            AppName: name,
+            AppVersion: version,
+            AppPublisher: publisher,
+            AppPublisherURL: url,
+            AppSupportURL: url,
+            AppUpdatesURL: url,
+            DefaultDirName: defaultDirName,
+            DisableDirPage: "yes",
+            DisableProgramGroupPage: "yes",
+            OutputBaseFilename: "install",
+            Compression: "lzma",
+            SolidCompression: "yes",
+            WizardStyle: "modern",
+        }).map(x => `${x[0]}=${x[1]}`).join("\n");
+        const iss = {
+            setup,
+            languages: INNO_LANGUAGES_SECTION
+        };
+        const { code, files } = this.data;
+        if (files != null) {
+            iss.files = files.build();
+        }
+        if (code != null) {
+            iss.code = code.build();
+        }
+        let out = `[Setup]\n${iss.setup}\n\n[Languages]\n${iss.languages}\n\n`;
+        if (iss.files != null) {
+            out += `[Files]\n${iss.files}\n\n`;
+        }
+        if (iss.code != null) {
+            out += `[Code]\n${iss.code}\n\n`;
+        }
+        return out;
+    }
+    write(filePath) {
+        fs_1.default.writeFileSync(filePath, this.build(), "utf8");
+    }
+}
+exports.InnoSetupBuilder = InnoSetupBuilder;
+const INNO_LANGUAGES_SECTION = `\
+Name: "english"; MessagesFile: "compiler:Default.isl"
+Name: "armenian"; MessagesFile: "compiler:Languages\\Armenian.isl"
+Name: "brazilianportuguese"; MessagesFile: "compiler:Languages\\BrazilianPortuguese.isl"
+Name: "catalan"; MessagesFile: "compiler:Languages\\Catalan.isl"
+Name: "corsican"; MessagesFile: "compiler:Languages\\Corsican.isl"
+Name: "czech"; MessagesFile: "compiler:Languages\\Czech.isl"
+Name: "danish"; MessagesFile: "compiler:Languages\\Danish.isl"
+Name: "dutch"; MessagesFile: "compiler:Languages\\Dutch.isl"
+Name: "finnish"; MessagesFile: "compiler:Languages\\Finnish.isl"
+Name: "french"; MessagesFile: "compiler:Languages\\French.isl"
+Name: "german"; MessagesFile: "compiler:Languages\\German.isl"
+Name: "hebrew"; MessagesFile: "compiler:Languages\\Hebrew.isl"
+Name: "icelandic"; MessagesFile: "compiler:Languages\\Icelandic.isl"
+Name: "italian"; MessagesFile: "compiler:Languages\\Italian.isl"
+Name: "japanese"; MessagesFile: "compiler:Languages\\Japanese.isl"
+Name: "norwegian"; MessagesFile: "compiler:Languages\\Norwegian.isl"
+Name: "polish"; MessagesFile: "compiler:Languages\\Polish.isl"
+Name: "portuguese"; MessagesFile: "compiler:Languages\\Portuguese.isl"
+Name: "russian"; MessagesFile: "compiler:Languages\\Russian.isl"
+Name: "slovak"; MessagesFile: "compiler:Languages\\Slovak.isl"
+Name: "slovenian"; MessagesFile: "compiler:Languages\\Slovenian.isl"
+Name: "spanish"; MessagesFile: "compiler:Languages\\Spanish.isl"
+Name: "turkish"; MessagesFile: "compiler:Languages\\Turkish.isl"
+Name: "ukrainian"; MessagesFile: "compiler:Languages\\Ukrainian.isl"
+`;
+const INNO_CODE_SECTION = `\
+[Code]
+function UninstallIfExists(sInput: String, sArgs: String): String;
+var
+  sUnInstPath: String;
+  sUnInstallString: String;
+  iResultCode: Integer;
+begin
+  sUnInstPath := 'Software\Microsoft\Windows\CurrentVersion\Uninstall\' + sInput;
+  sUnInstallString := '';
+  if not RegQueryStringValue(HKLM, sUnInstPath, 'UninstallString', sUnInstallString) then
+    RegQueryStringValue(HKCU, sUnInstPath, 'UninstallString', sUnInstallString);
+  if sUnInstallString <> '' then
+  begin
+    Exec(sUnInstallString, sArgs, '', SW_HIDE, ewWaitUntilTerminated, iResultCode);
+    if iResultCode <> 0 then
+    begin
+        Result := 'Failed to uninstall ' + sInput + ' (Error code: ' + IntToStr(iResultCode) + ')';
+    end;
+  end;
+end;
+
+procedure CurStepChanged(CurStep: TSetupStep);
+var
+    RunResult: String;
+begin
+    if CurStep = ssPostInstall then
+    begin
+        RunResult := RunPostInstall();
+        if RunResult <> '' then RaiseException(RunResult);
+    end;
+end;
+
+procedure CurUninstallStepChanged(CurUninstallStep: TUninstallStep);
+var
+    RunResult: String;
+begin
+    if CurUninstallStep = usUninstall then
+    begin
+        RunResult := RunPreUninstall();
+        if RunResult <> '' then RaiseException(RunResult);
+    end;
+end;
+
+function PrepareToInstall(var NeedsRestart: Boolean): String;
+begin
+    Result := RunPreInstall();
+end;
+`;
+function generateExec(binary, args, errorMsg) {
+    return `\
+if Result = '' then
+begin
+    Exec(ExpandConstant('${binary}'), ExpandConstant('${args}'), '', SW_HIDE, ewWaitUntilTerminated, iResultCode);
+    if iResultCode <> 0 then
+    begin
+        Result := '${errorMsg.replace(/'/g, "\\'")} (Error code: ' + IntToStr(iResultCode) + ')';
+    end;
+end;
+`;
+}
+function generateNsisUninst(productCode) {
+    return `\
+if Result = '' then
+begin
+    Result := UninstallIfExists('${productCode}');
+end;
+`;
+}
+class InnoSetupCodeBuilder {
+    constructor() {
+        this.preInstalls = [];
+        this.postInstalls = [];
+        this.preUninstalls = [];
+    }
+    uninstallLegacy(productCode, type) {
+        if (type === "nsis") {
+            this.preInstalls.push(generateNsisUninst(productCode));
+        }
+        else {
+            throw new Error(`Unhandled type: '${type}'`);
+        }
+        return this;
+    }
+    execPreInstall(binary, args, errorMsg) {
+        this.preInstalls.push(generateExec(binary, args, errorMsg));
+        return this;
+    }
+    execPostInstall(binary, args, errorMsg) {
+        this.postInstalls.push(generateExec(binary, args, errorMsg));
+        return this;
+    }
+    execPreUninstall(binary, args, errorMsg) {
+        this.preUninstalls.push(generateExec(binary, args, errorMsg));
+        return this;
+    }
+    generatePreInstall() {
+        const cmd = `\
+function RunPreInstall: String;
+var
+    iResultCode: Integer;
+begin
+${this.preInstalls.join("\n")}
+end;
+`;
+        return cmd;
+    }
+    generatePostInstall() {
+        const cmd = `\
+function RunPostInstall: String;
+var
+    iResultCode: Integer;
+begin
+${this.postInstalls.join("\n")}
+end;
+`;
+        return cmd;
+    }
+    generatePreUninstall() {
+        const cmd = `\
+function RunPreUninstall: String;
+var
+    iResultCode: Integer;
+begin
+${this.preUninstalls.join("\n")}
+end;
+`;
+        return cmd;
+    }
+    build() {
+        return `\
+${INNO_CODE_SECTION}
+${this.generatePreInstall()}
+${this.generatePostInstall()}
+${this.generatePreUninstall()}
+`;
+    }
+}
+class InnoSetupFilesBuilder {
+    constructor() {
+        this.files = [];
+    }
+    add(source, dest, flags) {
+        this.files.push({
+            Source: source,
+            DestDir: dest,
+            Flags: flags
+        });
+        return this;
+    }
+    build() {
+        return this.files.map(stringFromInnoFile).join("\n");
+    }
+}
---
+++ inno.ts
@@ -0,0 +1,315 @@
+import fs from "fs"
+
+type InnoFile = {
+    Source: string,
+    DestDir: string,
+    Flags?: string[]
+}
+
+function stringFromInnoFile(input: InnoFile): string {
+    let out = `Source: "${input.Source}"; DestDir: "${input.DestDir}"`
+
+    if (input.Flags) {
+        out += "; Flags: "
+        out += input.Flags.join(" ")
+    }
+
+    return out
+}
+
+export class InnoSetupBuilder {
+    private data: { [key: string]: any } = {}
+
+    name(input: string): InnoSetupBuilder {
+        this.data.name = input
+        return this
+    }
+
+    version(input: string): InnoSetupBuilder {
+        this.data.version = input
+        return this
+    }
+
+    publisher(input: string): InnoSetupBuilder {
+        this.data.publisher = input
+        return this
+    }
+
+    url(input: string): InnoSetupBuilder {
+        this.data.url = input
+        return this
+    }
+
+    productCode(input: string): InnoSetupBuilder {
+        this.data.productCode = input
+        return this
+    }
+
+    defaultDirName(input: string): InnoSetupBuilder {
+        this.data.defaultDirName = input
+        return this
+    }
+
+    files(callback: (builder: InnoSetupFilesBuilder) => InnoSetupFilesBuilder): InnoSetupBuilder {
+        this.data.files = callback(new InnoSetupFilesBuilder())
+        return this
+    }
+
+    code(callback: (builder: InnoSetupCodeBuilder) => InnoSetupCodeBuilder): InnoSetupBuilder {
+        this.data.code = callback(new InnoSetupCodeBuilder())
+        return this
+    }
+
+    build(): string {
+        for (const key of ["name", "version", "url", "productCode", "defaultDirName"]) {
+            if (this.data[key] == null) {
+                throw new Error(`Missing key "${key}" for Inno Setup builder`)
+            }
+        }
+        
+        const { name, version, publisher, url, productCode, defaultDirName } = this.data
+        const setup = Object.entries({
+            AppId: `{${productCode}`,
+            AppName: name,
+            AppVersion: version,
+            AppPublisher: publisher,
+            AppPublisherURL: url,
+            AppSupportURL: url,
+            AppUpdatesURL: url,
+            DefaultDirName: defaultDirName,
+            DisableDirPage: "yes",
+            DisableProgramGroupPage: "yes",
+            OutputBaseFilename: "install",
+            Compression: "lzma",
+            SolidCompression: "yes",
+            WizardStyle: "modern",
+        }).map(x => `${x[0]}=${x[1]}`).join("\n")
+
+        const iss: any = {
+            setup,
+            languages: INNO_LANGUAGES_SECTION
+        }
+
+        const { code, files } = this.data
+
+        if (files != null) {
+            iss.files = files.build()
+        }
+
+        if (code != null) {
+            iss.code = code.build()
+        }
+
+        let out = `[Setup]\n${iss.setup}\n\n[Languages]\n${iss.languages}\n\n`
+
+        if (iss.files != null) {
+            out += `[Files]\n${iss.files}\n\n`
+        }
+
+        if (iss.code != null) {
+            out += `[Code]\n${iss.code}\n\n`
+        }
+
+        return out
+    }
+
+    write(filePath: string) {
+        fs.writeFileSync(filePath, this.build(), "utf8")
+    }
+}
+
+
+const INNO_LANGUAGES_SECTION = `\
+Name: "english"; MessagesFile: "compiler:Default.isl"
+Name: "armenian"; MessagesFile: "compiler:Languages\\Armenian.isl"
+Name: "brazilianportuguese"; MessagesFile: "compiler:Languages\\BrazilianPortuguese.isl"
+Name: "catalan"; MessagesFile: "compiler:Languages\\Catalan.isl"
+Name: "corsican"; MessagesFile: "compiler:Languages\\Corsican.isl"
+Name: "czech"; MessagesFile: "compiler:Languages\\Czech.isl"
+Name: "danish"; MessagesFile: "compiler:Languages\\Danish.isl"
+Name: "dutch"; MessagesFile: "compiler:Languages\\Dutch.isl"
+Name: "finnish"; MessagesFile: "compiler:Languages\\Finnish.isl"
+Name: "french"; MessagesFile: "compiler:Languages\\French.isl"
+Name: "german"; MessagesFile: "compiler:Languages\\German.isl"
+Name: "hebrew"; MessagesFile: "compiler:Languages\\Hebrew.isl"
+Name: "icelandic"; MessagesFile: "compiler:Languages\\Icelandic.isl"
+Name: "italian"; MessagesFile: "compiler:Languages\\Italian.isl"
+Name: "japanese"; MessagesFile: "compiler:Languages\\Japanese.isl"
+Name: "norwegian"; MessagesFile: "compiler:Languages\\Norwegian.isl"
+Name: "polish"; MessagesFile: "compiler:Languages\\Polish.isl"
+Name: "portuguese"; MessagesFile: "compiler:Languages\\Portuguese.isl"
+Name: "russian"; MessagesFile: "compiler:Languages\\Russian.isl"
+Name: "slovak"; MessagesFile: "compiler:Languages\\Slovak.isl"
+Name: "slovenian"; MessagesFile: "compiler:Languages\\Slovenian.isl"
+Name: "spanish"; MessagesFile: "compiler:Languages\\Spanish.isl"
+Name: "turkish"; MessagesFile: "compiler:Languages\\Turkish.isl"
+Name: "ukrainian"; MessagesFile: "compiler:Languages\\Ukrainian.isl"
+`
+
+const INNO_CODE_HEADER = `\
+function UninstallIfExists(sInput: String; sArgs: String): String;
+var
+  sUnInstPath: String;
+  sUnInstallString: String;
+  iResultCode: Integer;
+begin
+  sUnInstPath := 'Software\Microsoft\Windows\CurrentVersion\Uninstall\' + sInput;
+  sUnInstallString := '';
+  if not RegQueryStringValue(HKLM, sUnInstPath, 'UninstallString', sUnInstallString) then
+    RegQueryStringValue(HKCU, sUnInstPath, 'UninstallString', sUnInstallString);
+  if sUnInstallString <> '' then
+  begin
+    Exec(sUnInstallString, sArgs, '', SW_HIDE, ewWaitUntilTerminated, iResultCode);
+    if iResultCode <> 0 then
+    begin
+        Result := 'Failed to uninstall ' + sInput + ' (Error code: ' + IntToStr(iResultCode) + ')';
+    end;
+  end;
+end;
+`
+
+const INNO_CODE_EVENTS = `\
+procedure CurStepChanged(CurStep: TSetupStep);
+var
+    RunResult: String;
+begin
+    if CurStep = ssPostInstall then
+    begin
+        RunResult := RunPostInstall();
+        if RunResult <> '' then RaiseException(RunResult);
+    end;
+end;
+
+procedure CurUninstallStepChanged(CurUninstallStep: TUninstallStep);
+var
+    RunResult: String;
+begin
+    if CurUninstallStep = usUninstall then
+    begin
+        RunResult := RunPreUninstall();
+        if RunResult <> '' then RaiseException(RunResult);
+    end;
+end;
+
+function PrepareToInstall(var NeedsRestart: Boolean): String;
+begin
+    Result := RunPreInstall();
+end;
+`
+
+function generateExec(binary: string, args: string, errorMsg: string) {
+    return `\
+if Result = '' then
+begin
+    Exec(ExpandConstant('${binary}'), ExpandConstant('${args}'), '', SW_HIDE, ewWaitUntilTerminated, iResultCode);
+    if iResultCode <> 0 then
+    begin
+        Result := '${errorMsg.replace(/'/g, "\\'")} (Error code: ' + IntToStr(iResultCode) + ')';
+    end;
+end;
+`
+}
+
+function generateNsisUninst(productCode: string) {
+    return `\
+if Result = '' then
+begin
+    Result := UninstallIfExists('${productCode}', '/S');
+end;
+`
+}
+
+class InnoSetupCodeBuilder {
+    private preInstalls: string[] = []
+    private postInstalls: string[] = []
+    private preUninstalls: string[] = []
+
+    uninstallLegacy(productCode: string, type: string): InnoSetupCodeBuilder {
+        if (type === "nsis") {
+            this.preInstalls.push(generateNsisUninst(productCode))
+        } else {
+            throw new Error(`Unhandled type: '${type}'`)
+        }
+
+        return this
+    }
+
+    execPreInstall(binary: string, args: string, errorMsg: string): InnoSetupCodeBuilder {
+        this.preInstalls.push(generateExec(binary, args, errorMsg))
+        return this
+    }
+
+    execPostInstall(binary: string, args: string, errorMsg: string): InnoSetupCodeBuilder {
+        this.postInstalls.push(generateExec(binary, args, errorMsg))
+        return this
+    }
+
+    execPreUninstall(binary: string, args: string, errorMsg: string): InnoSetupCodeBuilder {
+        this.preUninstalls.push(generateExec(binary, args, errorMsg))
+        return this
+    }
+
+    private generatePreInstall(): string {
+        const cmd = `\
+function RunPreInstall: String;
+var
+    iResultCode: Integer;
+begin
+${this.preInstalls.join("\n")}
+end;
+`
+        return cmd
+    }
+
+    private generatePostInstall() {
+        const cmd = `\
+function RunPostInstall: String;
+var
+    iResultCode: Integer;
+begin
+${this.postInstalls.join("\n")}
+end;
+`
+        return cmd
+    }
+
+    private generatePreUninstall() {
+const cmd = `\
+function RunPreUninstall: String;
+var
+    iResultCode: Integer;
+begin
+${this.preUninstalls.join("\n")}
+end;
+`
+        return cmd
+    }
+
+    build(): string {
+        return `\
+${INNO_CODE_HEADER}
+${this.generatePreInstall()}
+${this.generatePostInstall()}
+${this.generatePreUninstall()}
+${INNO_CODE_EVENTS}
+`
+    }
+}
+
+class InnoSetupFilesBuilder {
+    private files: InnoFile[] = []
+
+    // Source can be an absolute path, or relative to the .iss file
+    add(source: string, dest: string, flags?: string[]): InnoSetupFilesBuilder {
+        this.files.push({
+            Source: source,
+            DestDir: dest,
+            Flags: flags
+        })
+        return this
+    }
+
+    build(): string {
+        return this.files.map(stringFromInnoFile).join("\n")
+    }
+}
