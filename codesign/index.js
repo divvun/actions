def98728752a1d8fbf3df07dc546e29cf03fb108
Brendan Molloy <brendan@bbqsrc.net>
2020-08-23 21:53:32 +0200

    Try to add code signing for macOS

--- codesign/index.js
+++ codesign/index.js
@@ -6,12 +6,18 @@ var __importStar = (this && this.__importStar) || function (mod) {
     result["default"] = mod;
     return result;
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const core = __importStar(require("@actions/core"));
 const exec = __importStar(require("@actions/exec"));
+const path_1 = __importDefault(require("path"));
 const shared_1 = require("../shared");
+const delay = (ms) => new Promise((resolve) => setTimeout(() => resolve(), ms));
 async function run() {
-    const filePath = core.getInput('path', { required: true });
+    const filePath = path_1.default.resolve(core.getInput('path', { required: true }));
+    const fileName = filePath.split(path_1.default.sep).pop();
     const sec = shared_1.secrets();
     if (process.platform == "win32") {
         await exec.exec("signtool.exe", [
@@ -19,6 +25,43 @@ async function run() {
             "/f", shared_1.DIVVUN_PFX, "/p", sec.windows.pfxPassword,
             filePath
         ]);
+    }
+    else if (process.platform === "darwin") {
+        const { developerAccount, appPassword, appCodeSignId } = sec.macos;
+        await exec.exec("codesign", ["-s", appCodeSignId, filePath, "--timestamp", "--options=runtime"]);
+        const zipPath = path_1.default.resolve(path_1.default.dirname(filePath), "upload.zip");
+        await exec.exec("ditto", ["-c", "-k", "--keepParent", filePath, zipPath]);
+        const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+        const fakeBundleId = `com.github.${owner}.${repo}.${fileName}.zip`;
+        const response = JSON.parse((await shared_1.Bash.runScript(`xcrun altool --notarize-app\
+ --primary-bundle-id ${fakeBundleId}\
+ --username "${developerAccount}"\
+ --password "${appPassword}"\
+ --output-format json`)).join("\n"));
+        console.log(JSON.stringify(response, null, 2));
+        const requestUuid = response["notarization-upload"].RequestUUID;
+        for (;;) {
+            console.log("Waiting 10 seconds...");
+            await delay(10000);
+            console.log("Polling for status...");
+            const response = JSON.parse((await shared_1.Bash.runScript(`xcrun altool\
+ --notarization-info ${requestUuid}\
+ -u "${developerAccount}"\
+ -p ${appPassword}"\
+ --output-format json`)).join("\n"));
+            console.log(JSON.stringify(response, null, 2));
+            const status = response["notarization-info"].Status;
+            if (status === "success") {
+                console.log("Success!");
+                break;
+            }
+            else if (status === "in progress") {
+                console.log("In progress...");
+            }
+            else {
+                throw new Error(`Got failure status: ${status}`);
+            }
+        }
     }
     else {
         throw new Error("Unsupported platform: " + process.platform);
--- codesign/index.ts
+++ codesign/index.ts
@@ -1,10 +1,15 @@
 import * as core from '@actions/core'
 import * as exec from '@actions/exec'
 
-import { secrets, DIVVUN_PFX } from "../shared"
+import path from "path"
+
+import { secrets, DIVVUN_PFX, Bash } from "../shared"
+
+const delay = (ms: number) => new Promise((resolve) => setTimeout(() => resolve(), ms))
 
 async function run() {
-    const filePath = core.getInput('path', { required: true })
+    const filePath = path.resolve(core.getInput('path', { required: true }))
+    const fileName = filePath.split(path.sep).pop()
     const sec = secrets()
 
     if (process.platform == "win32") {
@@ -13,6 +18,54 @@ async function run() {
             "/f", DIVVUN_PFX, "/p", sec.windows.pfxPassword,
             filePath
         ])
+    } else if (process.platform === "darwin") {
+        const { developerAccount, appPassword, appCodeSignId } = sec.macos
+
+        // Codesign with hardened runtime and timestamp
+        await exec.exec("codesign", ["-s", appCodeSignId, filePath, "--timestamp", "--options=runtime"])
+
+        // Do some notarization
+        const zipPath = path.resolve(path.dirname(filePath), "upload.zip")
+
+        // Create zip file the way that Apple demands
+        await exec.exec("ditto", ["-c", "-k", "--keepParent", filePath, zipPath])
+
+        // Upload the zip
+        const [owner, repo] = process.env.GITHUB_REPOSITORY!.split("/")
+        const fakeBundleId = `com.github.${owner}.${repo}.${fileName}.zip`
+        const response: any = JSON.parse((await Bash.runScript(`xcrun altool --notarize-app\
+ --primary-bundle-id ${fakeBundleId}\
+ --username "${developerAccount}"\
+ --password "${appPassword}"\
+ --output-format json`)).join("\n"))
+        console.log(JSON.stringify(response, null, 2))
+
+        const requestUuid = response["notarization-upload"].RequestUUID
+        
+        // Poll API endpoint for response
+        for (;;) {
+            console.log("Waiting 10 seconds...")
+            await delay(10000)
+            console.log("Polling for status...")
+
+            const response: any = JSON.parse((await Bash.runScript(`xcrun altool\
+ --notarization-info ${requestUuid}\
+ -u "${developerAccount}"\
+ -p ${appPassword}"\
+ --output-format json`)).join("\n"))
+            console.log(JSON.stringify(response, null, 2))
+            
+            const status = response["notarization-info"].Status
+
+            if (status === "success") {
+                console.log("Success!")
+                break
+            } else if (status === "in progress") {
+                console.log("In progress...")
+            } else {
+                throw new Error(`Got failure status: ${status}`)
+            }
+        }
     } else {
         throw new Error("Unsupported platform: " + process.platform)
     }
