84a135d91e8e59c324ac305e052a4720e35e1d4c
Brendan Molloy <brendan@bbqsrc.net>
2020-09-21 14:16:40 +0200

    Update node_modules

--- inno.js
+++ inno.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.InnoSetupBuilder = void 0;
 const fs_1 = __importDefault(require("fs"));
 function stringFromInnoFile(input) {
     let out = `Source: "${input.Source}"; DestDir: "${input.DestDir}"`;
