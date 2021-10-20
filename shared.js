850bb2b6975283d790e3cc61892a773e21c24c69
Bastien Orivel <bastien@technocreatives.com>
2021-10-20 15:12:38 +0200

    Compile the typescript and commit it because otherwise nothing matters...

--- shared.js
+++ shared.js
@@ -299,9 +299,9 @@ class PahkatPrefix {
     }
 }
 exports.PahkatPrefix = PahkatPrefix;
-PahkatPrefix.URL_LINUX = "https://pahkat.uit.no/artifacts/pahkat-prefix-cli_0.1.0-nightly.20211019T124649Z_linux_x86_64.txz";
+PahkatPrefix.URL_LINUX = "https://pahkat.uit.no/artifacts/pahkat-prefix-cli_0.1.0-nightly.20211019T150611Z_linux_x86_64.txz";
 PahkatPrefix.URL_MACOS = "https://pahkat.uit.no/artifacts/pahkat-prefix-cli_0.1.0-nightly.20211019T124649Z_macos_x86_64.txz";
-PahkatPrefix.URL_WINDOWS = "https://pahkat.uit.no/artifacts/pahkat-prefix-cli_0.1.0-nightly.20211019T124649Z_windows_i686.txz";
+PahkatPrefix.URL_WINDOWS = "https://pahkat.uit.no/artifacts/pahkat-prefix-cli_0.1.0-nightly.20211019T150611Z_windows_i686.txz";
 var MacOSPackageTarget;
 (function (MacOSPackageTarget) {
     MacOSPackageTarget["System"] = "system";
